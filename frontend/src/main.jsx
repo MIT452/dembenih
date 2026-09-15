9a640f96d97ed7b5771811b33d2c968ebb0189cc
MIT452 <madagascarinstituteoftech@gmail.com>
2026-09-15 11:54:18 +0000

    main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './responsive-overrides.css'
+import './glass-theme.css'
 import { AuthProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
